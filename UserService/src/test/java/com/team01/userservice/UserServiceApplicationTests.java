6a35da9dc6d581587f19cb65645a96183ca9e4dd
SandyFred <Sandy.Fred@ibm.com>
2021-03-25 11:47:19 +0530

    Created Project

---
+++ UserService/src/test/java/com/team01/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team01.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
